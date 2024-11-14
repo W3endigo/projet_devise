0ae7d091aa25393dc71a21d40ae521daf3b59646
w3endigo <git.unthread684@passmail.net>
2024-11-14 12:51:05 +0100

    🎉

---
+++ src/main/java/isen/projet_devise/ProjetDeviseApplication.java
@@ -0,0 +1,13 @@
+package isen.projet_devise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDeviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDeviseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isen/projet_devise/ProjetDeviseApplicationTests.java
@@ -0,0 +1,13 @@
+package isen.projet_devise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDeviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
